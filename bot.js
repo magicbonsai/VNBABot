d6375a2ff2cc81f77c275454c1ea61096a1a9a1d
Stan Wu Huang <shuang2831@gmail.com>
2021-12-25 14:26:45 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,6 +30,11 @@ const runRojWithIndexCheck = (teams, index) => {
 // Router + Express Setup
 
 const app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
