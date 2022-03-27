3026848985036373fc1bfd6716b0f5768cc67f49
Alex Zhu <alexzeyanzhu@gmail.com>
2022-03-26 20:38:25 -0500

    added some more comments

--- app/router/utils.js
+++ app/router/utils.js
@@ -1,5 +1,8 @@
 const { updateJSON } = require('../bots/rojBot');
 
+
+// Assumes the keys arrays have these as items: { key: string, value: number }
+
 const bulkUpdateJSON = (data, attributeKeys, tendencyKeys, badgeKeys) => {
   const dataWithUpdatedAttr = attributeKeys.reduce((acc, curr) => {
     return updateJSON("ATTRIBUTES", acc, curr);
